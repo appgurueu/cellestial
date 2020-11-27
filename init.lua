63459c43c4abafd983930fb832484d23e9e79197
Lars Mueller <appgurulars@gmx.de>
2020-11-27 16:43:42 +0100

    Fix crash if cellestiall is not enabled

--- init.lua
+++ init.lua
@@ -4,11 +4,16 @@ if not minetest.features.area_store_persistent_ids then
     error("Cellestial requires persistent area store IDs, upgrade to Minetest 5.1 or newer")
 end
 mod.extend("conf")
-local cellestiall_init = mod.get_resource("cellestiall", "init.lua")
+local cellestiall_init = minetest.get_modpath"cellestiall"
+if cellestiall_init then
+    cellestiall_init = cellestiall_init .. "/init.lua"
+end
 if cellestiall and modlib.file.exists(cellestiall_init) then
     dofile(cellestiall_init)
 end
 mod.extend("main")
 cellestial.arena = mod.loadfile_exports(mod.get_resource("arena.lua"))
 mod.extend("chatcommands")
-cellestiall.after_cellestial_loaded()
+if cellestiall then
+    cellestiall.after_cellestial_loaded()
+end
